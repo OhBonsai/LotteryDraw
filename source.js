5c632f33c740385580fecb45a248b147b3052a1c
OhBonsai <letbonsaibe@gmail.com>
2017-01-14 19:30:03 +0800

    add group rectangle chaos style

--- source.js
+++ source.js
@@ -3,14 +3,14 @@
  */
 var camera, scene, renderer;
 var controls;
-var objs = [];
-var targets=  {table:[], helix:[]};
+var notDrawObjs = [];
+var notDraws = [];
+data.forEach(function(o,i){notDraws[i] = o});
 
-// for creating table
-var groupMark = [0, 0, 0, 0, 0, 0];
 
 init();
 animate();
+onWindowResize();
 
 function init(){
     // init 3d env
@@ -36,7 +36,8 @@ function init(){
     data.forEach(function(entity){
         var card = document.createElement('div');
         card.className = 'card';
-        card.style.backgroundColor = groupColor[entity[2]] + (Math.random()*0.5+0.25) + ')';
+        // card.style.backgroundColor = groupColor[entity[2]] + (Math.random()*0.5+0.25) + ')';
+        card.style.backgroundColor = groupColor[entity[2]] + 1 + ')';
 
         var name = document.createElement('div');
         name.className = 'name';
@@ -53,27 +54,23 @@ function init(){
         curObj.position.y = Math.random() * 3000 - 1500;
         curObj.position.z = Math.random() * 2000 - 1500;
         scene.add(curObj);
-
-        objs.push(curObj);
+        notDrawObjs.push(curObj);
     });
-
-    // init table
-    data.forEach(function(entity){
-        var curObj = new THREE.Object3D();
-        var curIndex = groupMark[entity[2]]++;
-        var curGrid = entity[2];
-        curObj.position.x = (curIndex%20)*140 - 1260;
-        curObj.position.y = curGrid*600 - parseInt(curIndex/20)*180 - 1000;
-        targets.table.push(curObj)
-    });
-
 
     // button listener
-    document.getElementById('table').addEventListener('click', function(){
-        transform(targets.table, 2000);
+    document.getElementById('group').addEventListener('click', function(){
+        transform(toGroup(notDraws), 2000);
     }, false);
 
-    onWindowResize();
+    document.getElementById('chaos').addEventListener('click', function(){
+        transform(toRandom(notDraws), 2000);
+    }, false);
+
+    document.getElementById('rectangle').addEventListener('click', function(){
+        transform(toRectangle(notDraws), 2000);
+    }, false);
+
+
 }
 
 
@@ -86,16 +83,16 @@ function animate(){
 
 function transform(targets, duration){
     TWEEN.removeAll();
-    objs.forEach(function(curObj, idx){
+    notDrawObjs.forEach(function(curObj, idx){
         var target = targets[idx];
 
         new TWEEN.Tween(curObj.position)
-            .to({x: target.position.x, y: target.position.y, z: target.position.z}, Math.random() * duration + duration)
+            .to({x: target.x, y: target.y, z: target.z}, Math.random() * duration + duration)
             .easing(TWEEN.Easing.Exponential.InOut)
             .start();
 
         new TWEEN.Tween(curObj.rotation)
-            .to({x: target.rotation.x, y: target.rotation.y, z: target.rotation.z}, Math.random() * duration + duration)
+            .to({x: 0, y: 0, z: 0}, Math.random() * duration + duration)
             .easing(TWEEN.Easing.Exponential.InOut)
             .start();
     });
@@ -118,4 +115,61 @@ function onWindowResize(){
 function render(){
     renderer.render(scene, camera);
 }
+
+function toGroup(notDrawList){
+    var tableTargets = [];
+    var groupMark = [0, 0, 0, 0, 0, 0];
+    notDrawList.forEach(function(entity){
+        var position = {};
+        var curIndex = groupMark[entity[2]]++;
+        var curGrid = entity[2];
+        position.x = (curIndex%20)*140 - 1260;
+        position.y = curGrid*600 - parseInt(curIndex/20)*180 - 1000;
+        position.z = 0;
+        tableTargets.push(position)
+    });
+    return tableTargets
+}
+
+
+function toRectangle(notDrawList){
+    var rectangleTargets = [];
+    notDrawList.forEach(function(entity, idx){
+        var position = {};
+        position.x = (idx%20)*140 - 1260;
+        position.y = parseInt(idx/20)*180 - 1000;
+        position.z = 0;
+        rectangleTargets.push(position)
+    });
+    return rectangleTargets
+}
+
+
+function toRandom(notDrawList){
+    var randomTargets = [];
+    notDrawList.forEach(function(){
+        var position = {};
+        position.x = Math.random() * 3000 - 1500;
+        position.y = Math.random() * 3000 - 1500;
+        position.z = Math.random() * 2000 - 1500;
+        randomTargets.push(position)
+    });
+    return randomTargets
+}
+
+function toThree(notDrawList){
+    var threeTargets = [];
+    notDrawList.forEach(function(entity){
+        var curObj = new THREE.Object3D();
+        var curIndex = groupMark[entity[2]]++;
+        var curGrid = entity[2];
+        curObj.position.x = (curIndex%20)*140 - 1260;
+        curObj.position.y = curGrid*600 - parseInt(curIndex/20)*180 - 1000;
+
+        threeTargets.push(curObj)
+    });
+    return threeTargets
+}
+
+
 
